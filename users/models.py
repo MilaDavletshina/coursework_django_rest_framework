cdda0d5893ad9932b612cba7f3107f95fb6a57ab
ludmila davletshina <ludaplv@mail.ru>
2024-12-16 10:46:00 +0300

    добавлена модель пользователя  users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    tg_nik = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="TG",
+        help_text="Укажите TG ник",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Укажите аватар",
+    )
+    tg_chat_id = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="TG chat_id",
+        help_text="Укажите TG chat_id",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
