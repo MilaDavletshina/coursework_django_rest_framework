4bfa3c7d8e80271461329ee9fd50a15d37b13f6b
ludmila davletshina <ludaplv@mail.ru>
2024-12-16 16:12:14 +0300

    добавлен UserCreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
-
 from users.models import User
 from users.serializers import UserSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 
-class UserViewSet(ModelViewSet):
+class UserCreateAPIView(CreateAPIView):
+    """CRUD для регистрации пользователя."""
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
