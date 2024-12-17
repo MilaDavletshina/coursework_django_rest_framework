cc6dffc5864fc770574e241bc19ea604e5bcb9e9
ludmila davletshina <ludaplv@mail.ru>
2024-12-17 15:30:15 +0300

    изменены permissions

--- habits/views.py
+++ habits/views.py
@@ -1,9 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
 from habits.models import Habit
 from habits.serializers import HabitSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsOwner
 
 
 class HabitViewSet(ModelViewSet):
     """CRUD модели привычка"""
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_permissions(self):
+        """Ограничивает доступ модератору"""
+        if self.action != "create":
+            self.permission_classes = [IsOwner]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,9 @@
-from os import path
+from django.urls import path
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
-from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView
