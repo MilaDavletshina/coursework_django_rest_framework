ea06cfba11c60d16552c34b012bfe0cd9c68a375
ludmila davletshina <ludaplv@mail.ru>
2024-12-18 13:29:44 +0300

    добавлен serializers

---
+++ habits/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from habits.models import Habit
+from habits.validators import RelatedOrIsPleasantValidator
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    """Серилизатор модели привычка."""
+    validators = serializers.CharField(validators=[RelatedOrIsPleasantValidator()])
+
+    class Meta:
+        model = Habit
+        fields = "__all__"
