c8eccb0bb1a3561a8b86e70b45dba9f3abd15b0a
ludmila davletshina <ludaplv@mail.ru>
2024-12-18 13:31:19 +0300

    добавлен views

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from habits.models import Habit
+from habits.paginations import CustomPagination
+from habits.serializers import HabitSerializer
+from rest_framework.permissions import IsAuthenticated
+from users.permissions import IsOwner
 
-# Create your views here.
+
+class HabitViewSet(ModelViewSet):
+    """CRUD модели привычка"""
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = CustomPagination
+
+    def get_permissions(self):
+        """Ограничивает доступ модератору"""
+        if self.action != "create":
+            self.permission_classes = [IsOwner]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.save()
