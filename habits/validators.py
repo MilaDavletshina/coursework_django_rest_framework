c6ed88194697a1e0960acc9066fab5ce5ec14b8c
ludmila davletshina <ludaplv@mail.ru>
2024-12-18 15:49:18 +0300

    добавлены валидаторы

--- habits/validators.py
+++ habits/validators.py
@@ -1,25 +1,43 @@
-# from datetime import timedelta
-#
-# from rest_framework.serializers import ValidationError
-#
-#
-# class RelatedOrIsPleasantValidator:
-#     """Исключение одновременного выбора связанной привычки и указания вознаграждения"""
-#     def __init__(self, related_habit):
-#         self.related_habit = related_habit
-#
-#     def __call__(self, value):
-#         habit = value.get(self.related_habit)
-#         if habit:
-#             if not habit.is_pleasant:
-#                 raise ValidationError("Не может быть заполнено одновременно и поле вознаграждения, и поле связанной привычки. Можно заполнить только одно из двух полей.")
-#
-#
-# class TimeLimiter:
-#     def __init__(self, execution_time):
-#         self.execution_time = execution_time
-#
-#     def __call__(self, value):
-#         time = value.get(self.execution_time)
-#         if time > timedelta(seconds=120):
-#             raise ValidationError("Время выполнения должно быть не больше 120 секунд")
+from datetime import timedelta
+from rest_framework.serializers import ValidationError
+
+
+class FillingTheFieldValidator:
+    """Проверка наличия у приятной привычки связанной привычки или вознаграждения"""
+
+    def __init__(self, is_pleasant, related_habit, remuneration):
+        self.is_pleasant = is_pleasant
+        self.related_habit = related_habit
+        self.remuneration = remuneration
+
+    def __call__(self, value):
+        is_pleasant_field = value.get(self.is_pleasant)
+        related_habit_field = value.get(self.related_habit)
+        remuneration_field = value.get(self.remuneration)
+
+        if (is_pleasant_field and related_habit_field) or (is_pleasant_field and remuneration_field):
+            raise ValidationError("У приятной привычки не может быть связанной привычки или вознаграждения")
+
+
+class RelatedOrIsPleasantValidator:
+    """Проверка связанной привычки на признак приятной привычки"""
+    def __init__(self, related_habit):
+        self.related_habit = related_habit
+
+    def __call__(self, value):
+        habit = value.get(self.related_habit)
+
+        if habit:
+            if not habit.is_pleasant:
+                raise ValidationError("Связанная привычка должна иметь признак приятной привычки.")
+
+
+class TimeLimiter:
+
+    def __init__(self, execution_time):
+        self.execution_time = execution_time
+
+    def __call__(self, value):
+        time = value.get(self.execution_time)
+        if time > timedelta(seconds=120):
+            raise ValidationError("Время выполнения должно быть не больше 120 секунд")
