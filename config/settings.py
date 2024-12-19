b05113f6798388d3bdd8273716c192c6c749029a
ludmila davletshina <ludaplv@mail.ru>
2024-12-19 11:58:34 +0300

    изменены настройки cors

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,13 @@ EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
-CORS_ALLOWED_ORIGINS = ['*',] # Замените на адрес вашего фронтенд-сервера
-CSRF_TRUSTED_ORIGINS = ['https://read-and-write.example.com',  # Замените на адрес вашего фронтенд-сервера
-                        'http://127.0.0.1:8000/', # адрес бэкенд-сервера
-                        ]
-CORS_ALLOW_ALL_ORIGINS = False
+# адреса от frontend
+CORS_ALLOWED_ORIGINS = [
+    "https://read-only.example.com",
+    "https://read-and-write.example.com",
+]
+
+# адреса от backend
+CSRF_TRUSTED_ORIGINS = [
+    "https://read-and-write.example.com",
+]
