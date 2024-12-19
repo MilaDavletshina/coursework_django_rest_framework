c90ed5cbb8b43886fc2def6c30031b001296016a
ludmila davletshina <ludaplv@mail.ru>
2024-12-19 13:03:40 +0300

    app_name = HabitsConfig.name

--- habits/urls.py
+++ habits/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from habits.apps import HabitsConfig
 from habits.views import HabitsListViewSet, HabitViewSet, PublicHabitListView
 
-app_name = HabitsConfig
+app_name = HabitsConfig.name
 
 router = DefaultRouter()
 router.register(r"habit", HabitViewSet, basename="habit")
