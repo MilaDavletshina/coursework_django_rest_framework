1090daa30367136e7cb3e75cda9684419c31cdf5
ludmila davletshina <ludaplv@mail.ru>
2025-01-19 12:18:07 +0300

    форматировано линтерами

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import UserSerializer
